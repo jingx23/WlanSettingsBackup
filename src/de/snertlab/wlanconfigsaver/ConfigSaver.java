fdc340526ffd04b7769eabe8b3b5c80d534c7766
snert <snert@snert-lab.de>
2011-01-15 02:31:56 +0100

    changed from cp to cat => seems to be no permission problem anymore

--- src/de/snertlab/wlanconfigsaver/ConfigSaver.java
+++ src/de/snertlab/wlanconfigsaver/ConfigSaver.java
@@ -17,6 +17,8 @@ public class ConfigSaver extends Activity {
 	private static final String BACKUP_FILENAME = "wpa_supplicant.conf.bak";
 	private static final String BACKUP_PATH = "/sdcard/" + BACKUP_FILENAME;
 	
+	private String catpath;
+	
 	private TextView txtViewInfo;
 	
     @Override
@@ -24,16 +26,20 @@ public class ConfigSaver extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         txtViewInfo = (TextView) findViewById(R.id.TextView01);
+        doInit();
+    }
+    
+    private void doInit(){
+    	boolean catPathFound = findCatPath();
+    	StringBuilder sb = new StringBuilder();
+    	sb.append("Init Status:\n");
+    	sb.append("cat path found: " + catPathFound + "\n");
+    	sb.append("is root: " + hazIGotRoot() + "\n");
+    	txtViewInfo.setText(sb.toString());
     }
     
     public void btnClickHandlerBackup(View view) throws IOException, InterruptedException {
-    	Runtime r=Runtime.getRuntime();
-    	Process p2 = r.exec("su");
-    	DataOutputStream d=new DataOutputStream(p2.getOutputStream());
-    	d.writeBytes("cp " + WPA_SUPPLICANT_PATH + " " + BACKUP_PATH + "\n");
-    	d.writeBytes("exit\n");
-    	d.flush();
-    	p2.waitFor();
+    	runAsRoot(catpath + " " + WPA_SUPPLICANT_PATH + " > " + BACKUP_PATH + "\n");
     	File file = new File(BACKUP_PATH);
     	txtViewInfo.setText("backup " + (file.exists() ? "successful " + BACKUP_PATH : "failed"));
     }
@@ -44,15 +50,44 @@ public class ConfigSaver extends Activity {
     		txtViewInfo.setText("backup file :" + BACKUP_PATH + " not found");
     		return;
     	}
+    	runAsRoot(catpath + " " + BACKUP_PATH + " > " + WPA_SUPPLICANT_PATH + "\n");
+    	txtViewInfo.setText("restore successful"); //TODO: Richtige Prüfung einbauen ob successful oder nicht
+    }
+    
+    public boolean findCatPath() {
+    	String[] arrayOfString = new String[4];
+    	arrayOfString[0] = "/system/bin/cat";
+    	arrayOfString[1] = "/system/xbin/cat";
+    	arrayOfString[2] = "/data/busybox/cat";
+    	arrayOfString[3] = "/system/xbin/bb/cat";
+    	for (int i = 0; i < arrayOfString.length; i++) {
+    		String path = arrayOfString[i];
+    		File localFile = new File(path);
+    		if ((localFile.exists()) || (localFile.isFile())){
+    			catpath = path;
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    private boolean hazIGotRoot(){
+    	return runAsRoot("");
+    }
+    
+    private boolean runAsRoot(String command){
+    	try{
     	Runtime r=Runtime.getRuntime();
     	Process p2 = r.exec("su");
     	DataOutputStream d=new DataOutputStream(p2.getOutputStream());
-    	d.writeBytes("cp " + BACKUP_PATH + " " + WPA_SUPPLICANT_PATH + "\n");
-    	d.writeBytes("chmod u=rw,g=rw,o=rw " + WPA_SUPPLICANT_PATH + "\n");
-    	//d.writeBytes("chown system:wifi " + WPA_SUPPLICANT_PATH + "\n"); //funzt leider nicht system unknown user
+    	d.writeBytes(command);
     	d.writeBytes("exit\n");
     	d.flush();
-    	p2.waitFor();
-    	txtViewInfo.setText("restore successful"); //TODO: Richtige Prüfung einbauen ob successful oder nicht
+    	int retval = p2.waitFor();
+    	return (retval==0);
+    	}catch(Exception e){
+    		//TODO: logging
+    		return false;
+    	}
     }
 }
