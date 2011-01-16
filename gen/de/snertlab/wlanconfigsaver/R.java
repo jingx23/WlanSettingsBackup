4af5bce8dcc8dc9ea9b834640e8783f12b470fcb
snert <snert@snert-lab.de>
2011-01-16 18:54:10 +0100

    added donation text => currently hidden

--- gen/de/snertlab/wlanconfigsaver/R.java
+++ gen/de/snertlab/wlanconfigsaver/R.java
@@ -17,6 +17,7 @@ public final class R {
         public static final int Button01=0x7f050000;
         public static final int Button02=0x7f050001;
         public static final int TextView01=0x7f050002;
+        public static final int TextViewProgInfo=0x7f050003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
